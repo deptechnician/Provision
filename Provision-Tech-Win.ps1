662596912fb05879d7d45caf19d4ef5755c99c1b
Barry Wilson <deptechnician@toastymails.com>
2022-12-27 20:32:54 -0700

    First Commit

--- Provision-Tech-Win.ps1
+++ Provision-Tech-Win.ps1
@@ -1,6 +1,6 @@
 # Install Chocolatey
 #
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install firefox
 #   Once installed, sign in with deptechnician@toastymails.com for baseline setup of the browser
