074b68c588772a975b9242a40562e0a8f930216f
Barry Wilson <deptechnician@toastymails.com>
2022-12-27 20:23:13 -0700

    Technician workspace provisioining

---
+++ Provision-Tech-Win.ps1
@@ -0,0 +1,20 @@
+# Install Chocolatey
+#
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install firefox
+#   Once installed, sign in with deptechnician@toastymails.com for baseline setup of the browser
+#
+choco install firefox
+
+# Install the other technician tools
+#
+choco install git vnc-viewer keepass vscode
+
+# Download the realvnc instant help disposable app
+# 
+$InstantHelp = "$env:userprofile\Downloads\InstantHelp.exe"
+Invoke-WebRequest -Uri https://static.realvnc.help/generic/1.0.3/Instant-support.exe -OutFile $InstantHelp
+
+
+
